3609251e3d84642f34ff36987e5cfde3eed8f599
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-25 20:20:41 -0500

    start writing HeadersComponent function inside HeadersComponents.jsx

--- developerportfolio/src/Components/HeaderComponents/HeadersComponent.jsx
+++ developerportfolio/src/Components/HeaderComponents/HeadersComponent.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Box, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(() => ({
+    HeaderWrapper:{
+        color: "red"
+    }
+}))
 
 function HeadersComponent() {
-  return (
-      <div>
-      Header Works
-  </div>
-  ) 
+    const classes = useStyles()
+    return (
+        <Box className={classes.HeaderWrapper}>
+           Hello From Header Wrapper
+        </Box>
+    )
 }
 
 export default HeadersComponent;
