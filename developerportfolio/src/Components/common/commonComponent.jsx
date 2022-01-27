a8696e7782508f2859c812318abedb864b868113
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-27 12:42:03 -0500

    write Divider function inside commonComponent.jsx for underline design for About section

--- developerportfolio/src/Components/common/commonComponent.jsx
+++ developerportfolio/src/Components/common/commonComponent.jsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@material-ui/core"
 import { useStyles } from "../HeaderComponents/HeaderStyles"
 
 
-export const Decorator = ({ label, withIcon, Icon }) => {
+export const Decorator = ({ label, withIcon, Icon, styles }) => {
     const classes = useStyles();
     return (
         <Box className={classes.decorator}>
@@ -18,6 +18,16 @@ export const Decorator = ({ label, withIcon, Icon }) => {
     );
 };
 
-export const RenderSectionHeading = () => {
-    
+export const Divider = () => {
+    const classes = useStyles();
+    return <Typography variant="span" className={classes.divider}></Typography>
+}
+
+export const RenderSectionHeading = ({smallTxt, heading}) => {
+    //decorator
+    //heading
+    //line
+    //Description
+    Decorator({label:smallTxt, withIcon:false });
+    <Typography variant="h4" >{heading}</Typography>
 }
