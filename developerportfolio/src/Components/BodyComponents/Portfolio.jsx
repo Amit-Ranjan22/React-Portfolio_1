9ddaa527097db73328625f75d820055960a58db6
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-28 12:15:32 -0500

    write map function to render all the projects images in Portfolio.jsx

--- developerportfolio/src/Components/BodyComponents/Portfolio.jsx
+++ developerportfolio/src/Components/BodyComponents/Portfolio.jsx
@@ -3,17 +3,44 @@ import React from 'react';
 import { RenderSectionHeading } from '../common/commonComponent';
 // import { useStyles } from '../HeaderComponents/HeaderStyles';
 import { useStyles } from "./BodyStyles";
+import image1 from "../../images/5-2.jpg"
+import image2 from "../../images/5-3.jpg"
+import image3 from "../../images/5-4.jpg"
+import image4 from "../../images/5-6.jpg"
+import image5 from "../../images/5-8.jpg"
+import image6 from "../../images/5-9.jpg"
+import image7 from "../../images/5-2.jpg"
+import image8 from "../../images/5-3.jpg"
+import image9 from "../../images/5-4.jpg"
+import image10 from "../../images/5-6.jpg"
+import image11 from "../../images/5-8.jpg"
+import image12 from "../../images/5-9.jpg"
 
 export default function Portfolio() {
     const classes = useStyles()
+
+    const portfolioData = [
+        {url:image1,title:"# Portfolio",link:""},
+        {url:image2,title:"# Portfolio",link:""},
+        {url:image3,title:"# Portfolio",link:""},
+        {url:image4,title:"# Portfolio",link:""},
+        {url:image5,title:"# Portfolio",link:""},
+        {url:image6,title:"# Portfolio",link:""},
+        {url:image7,title:"# Portfolio",link:""},
+        {url:image8,title:"# Portfolio",link:""},
+        {url:image9,title:"# Portfolio",link:""},
+        {url:image10,title:"# Portfolio",link:""},
+        {url:image11,title:"# Portfolio",link:""},
+        {url:image12,title:"# Portfolio",link:""},
+    ]
     return (
         <Box className={classes.sectionDark} id="Portfolio">
-            <Grid 
-            container 
-            style={{
-                display:"flex", 
-                justifyContent:"center", 
-                alignContent:"center",
+            <Grid
+                container
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignContent: "center",
                 }}>
                 <Grid item xs={12} sm={8}>
                     {RenderSectionHeading({
@@ -23,6 +50,21 @@ export default function Portfolio() {
                     })}
                 </Grid>
             </Grid>
+            <container maxWidth="xl">
+                <Grid container spacing={2}>
+                    {portfolioData.map((item,i) => (
+                    <Grid item xs={6} sm={4} lg={3} key={i}>
+                        <Box className={classes.imageContainer}>
+                            <img
+                                src={item.url}
+                                alt={item.title}
+                                className={classes.responsiveImg} 
+                                />
+                        </Box>
+                    </Grid>
+                    ))}
+                </Grid>
+            </container>
         </Box>
     )
 }
